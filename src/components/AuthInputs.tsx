ef67c1517fb20bbac53aa27ab7f77bf69d7b74fe
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-10-31 19:57:59 +0530

    Add conditional inline styling

--- src/components/AuthInputs.tsx
+++ src/components/AuthInputs.tsx
@@ -27,7 +27,12 @@ export default function AuthInputs() {
           <label>Email</label>
           <input
             type="email"
-            className={emailNotValid ? "invalid" : undefined}
+            style={{
+              color: "#ef4444",
+              borderColor: "#f73f3f",
+              backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db",
+            }}
+            // className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
@@ -35,7 +40,12 @@ export default function AuthInputs() {
           <label>Password</label>
           <input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            style={{
+              color: "#ef4444",
+              borderColor: "#f73f3f",
+              backgroundColor: passwordNotValid ? "#fed2d2" : "#d1d5db",
+            }}
+            // className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
